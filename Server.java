238a1ca32797d645dd31c683a5954fc95be1fabd
wei <101487997+we1yuan@users.noreply.github.com>
2026-03-30 12:44:45 -0400

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,199 +1,164 @@
-
-
+import java.io.File;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.nio.file.Files;
-import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
+    private static final int SERVER_PORT = 8080;
+    private static final int BUFFER_SIZE = 65535;
+    private static final int SOCKET_TIMEOUT_MS = 20000;
+
     public static void main(String[] args) throws IOException {
-        // socket to send and recieve datagrams
-        DatagramSocket socket = new DatagramSocket(8080);
-        socket.setSoTimeout(5000);
+        DatagramSocket socket = new DatagramSocket(SERVER_PORT);
+        socket.setSoTimeout(0); // block while waiting for the first REQUEST
 
-        // recieve buffer
-        byte[] buffer = new byte[65535];
+        System.out.println("Server is listening on UDP port " + SERVER_PORT + ".");
 
-        // local ip address
-        InetAddress ipAddress = InetAddress.getLocalHost();
-
-        // Server flags
-        boolean sending = false;
-        boolean quiting = false;
-        boolean invalidID = false;
-
-        // File data
-        String[] fileData = new String[100];
-        fileData[0] = "";
-        int segmentToSend = 0;
-        int lastSegement = 0;
-
-        // validation numbers
-        int sequenceNumber = 0;
-        int connectionID = 0;
-
-        // payload size
-        int payloadSize = 0;
-
-        int retransmissions = 0;
-        long startTime = 0;
-        long endTime = 0;
-
-        // server runs until client sends quit
         while (true) {
-            while (!sending) {
-                // clear buffer
-                buffer = new byte[65535];
+            byte[] buffer = new byte[BUFFER_SIZE];
+            DatagramPacket requestDatagram = new DatagramPacket(buffer, buffer.length);
+            socket.receive(requestDatagram);
 
-                // create datagram shell
-                DatagramPacket datagram = new DatagramPacket(buffer, buffer.length);
-
-                // receive the datagram
-                try {
-                    socket.receive(datagram);
-
-                    // convert bytes to received packet
-                    Packet recievedPacket = Packet.stringToPacket(Packet.byteToString(buffer));
-
-                    // if message is a request, load the file to be sent
-                    if (recievedPacket.messageType.equals("REQUEST")) {
-                        File loadedFile = new File(recievedPacket.payload);
-                        // check that requested file is valid
-                        if (!loadedFile.canRead()) {
-                            fileData[0] = "";
-                        } else {
-                            System.out.println("Server recieved a valid REQUEST packet.");
-                            connectionID = recievedPacket.connectionID;
-                            sequenceNumber = recievedPacket.sequenceNumber;
-                            payloadSize = recievedPacket.payloadSize;
-
-                            byte[] fileBytes = Files.readAllBytes(loadedFile.toPath());
-
-                            // splits file bytes according to payload size
-                            int i = 0;
-                            int k = 0;
-                            while (i < fileBytes.length) {
-                                byte[] segmentBytes = new byte[payloadSize];
-                                int j = 0;
-                                while (j + 1 < payloadSize && i < fileBytes.length) {
-                                    segmentBytes[j] = fileBytes[i];
-                                    j++;
-                                    i++;
-                                }
-                                fileData[k] = Packet.byteToString(segmentBytes);
-                                lastSegement = k;
-                                k++;
-                            }
-                        }
-                    }
-/*
-                    // if message is a acknowledgement, handle it
-                    if (recievedPacket.messageType.equals("ACK")) {
-                        if (recievedPacket.connectionID == connectionID
-                                && recievedPacket.sequenceNumber == sequenceNumber) {
-                            // send next segment
-                            sequenceNumber = (sequenceNumber + 1) % 2;
-                            segmentToSend++;
-                            System.out.println("Server recieved a valid ACK packet.");
-                        } else if (recievedPacket.connectionID != connectionID) {
-                            // set error flag
-                            invalidID = true;
-                        } else {
-                            // else, resend last packet
-                            System.out.println("Server recieved an invalid ACK packet, resending last packet.");
-                        }
-                    }
-                } 
-*/              
-                    
-if (recievedPacket.messageType.equals("ACK")) {
-    if (recievedPacket.connectionID == connectionID &&
-        recievedPacket.sequenceNumber == sequenceNumber) {
-
-        System.out.println("Server recieved a valid ACK packet.");
-
-        if (segmentToSend == lastSegement) {
-            // 最后一个 ACK 收到后
-            endTime = System.currentTimeMillis();
-            System.out.println("Transfer time(ms): " + (endTime - startTime));
-            System.out.println("Retransmissions: " + retransmissions);
-
-            quiting = true;   // 最后一个 DATA 已被确认
-            break;
-        }
-
-        sequenceNumber = (sequenceNumber + 1) % 2;
-        segmentToSend++;
-    } else if (recievedPacket.connectionID != connectionID) {
-        invalidID = true;
-    } else {
-        System.out.println("Server recieved an invalid ACK packet, resending last packet.");
-    }
-}
-                } 
-                    catch (Exception e) {
-                    // timeout
-                    if (segmentToSend == lastSegement) {
-                        // last packet has been sent already, terminate connection
-                        quiting = true;
-                    } else {
-                        // else, resend last packet
-                        retransmissions++;
-                        System.out.println("Server timed out, resending last packet.");
-                    }
-                }
-
-                // swap mode
-                sending = true;
+            Packet requestPacket;
+            try {
+                requestPacket = Packet.stringToPacket(Packet.byteToString(requestDatagram.getData()));
+            } catch (Exception e) {
+                System.out.println("Server received a malformed packet. Packet discarded.");
+                continue;
             }
-            while (sending) {
-                // clear buffer
-                buffer = new byte[65535];
 
-                // create packet, assume packet failed until proven otherwise
-                Packet packet = new Packet(connectionID, sequenceNumber, "ERROR", payloadSize,
-                        "ERROR: File failed to load.",
-                        0);
-                if (invalidID) {
-                    packet.payload = "ERROR: Invalid connection ID.";
-                    invalidID = false;
-                } else if (fileData[0] != null && !fileData[0].isEmpty()) {
-                    // send ERROR otherwise
-                    packet.messageType = "DATA";
-                    packet.payload = fileData[segmentToSend];
-                    if (segmentToSend == lastSegement) {
-                        packet.last = 1;
-                    }
-                }
+            if (!"REQUEST".equals(requestPacket.messageType)) {
+                System.out.println("Server received a non-REQUEST packet while idle. Packet discarded.");
+                continue;
+            }
 
-                // load packet as bytes
-                buffer = Packet.packetToString(packet).getBytes();
+            InetAddress clientAddress = requestDatagram.getAddress();
+            int clientPort = requestDatagram.getPort();
+            int connectionID = requestPacket.connectionID;
+            int sequenceNumber = requestPacket.sequenceNumber;
+            int payloadSize = requestPacket.payloadSize;
 
-                // create datagram
-                DatagramPacket datagram = new DatagramPacket(buffer, buffer.length, ipAddress, 8081);
+            System.out.println("Server recieved a valid REQUEST packet.");
 
-                if (packet.messageType.equals("DATA") && segmentToSend == 0 && startTime == 0) {
+            if (payloadSize <= 0) {
+                Packet errorPacket = new Packet(connectionID, sequenceNumber, "ERROR", 0,
+                        "ERROR: Invalid payload size.", 0);
+                sendPacket(socket, errorPacket, clientAddress, clientPort);
+                System.out.println("Server sent a ERROR packet.");
+                continue;
+            }
+
+            File loadedFile = new File(requestPacket.payload);
+            if (!loadedFile.canRead()) {
+                Packet errorPacket = new Packet(connectionID, sequenceNumber, "ERROR", payloadSize,
+                        "ERROR: File failed to load.", 0);
+                sendPacket(socket, errorPacket, clientAddress, clientPort);
+                System.out.println("Server sent a ERROR packet.");
+                continue;
+            }
+
+            byte[] fileBytes = Files.readAllBytes(loadedFile.toPath());
+            List<String> fileData = splitFileIntoSegments(fileBytes, payloadSize);
+            int lastSegment = fileData.size() - 1;
+            int segmentToSend = 0;
+            int retransmissions = 0;
+            long startTime = 0;
+            long endTime = 0;
+
+            socket.setSoTimeout(SOCKET_TIMEOUT_MS);
+
+            while (true) {
+                Packet dataPacket = new Packet(
+                        connectionID,
+                        sequenceNumber,
+                        "DATA",
+                        payloadSize,
+                        fileData.get(segmentToSend),
+                        segmentToSend == lastSegment ? 1 : 0);
+
+                if (segmentToSend == 0 && startTime == 0) {
                     startTime = System.currentTimeMillis();
                 }
 
-                
-                // send datagram
-                socket.send(datagram);
-                if (packet.last == 0)
-                    System.out.println("Server sent a " + packet.messageType + " packet.");
-                else if (!quiting)
+                sendPacket(socket, dataPacket, clientAddress, clientPort);
+                if (dataPacket.last == 0) {
+                    System.out.println("Server sent a DATA packet.");
+                } else {
                     System.out.println("Server sent the last DATA packet.");
+                }
 
-                // swap mode
-                sending = false;
+                boolean resendCurrentSegment = false;
+
+                try {
+                    byte[] ackBuffer = new byte[BUFFER_SIZE];
+                    DatagramPacket ackDatagram = new DatagramPacket(ackBuffer, ackBuffer.length);
+                    socket.receive(ackDatagram);
+
+                    Packet receivedPacket = Packet.stringToPacket(Packet.byteToString(ackDatagram.getData()));
+
+                    if (!"ACK".equals(receivedPacket.messageType)) {
+                        System.out.println("Server recieved a non-ACK packet, resending last packet.");
+                        resendCurrentSegment = true;
+                    } else if (receivedPacket.connectionID != connectionID) {
+                        System.out.println("Server recieved an invalid connection ID, resending last packet.");
+                        resendCurrentSegment = true;
+                    } else if (receivedPacket.sequenceNumber != sequenceNumber) {
+                        System.out.println("Server recieved an invalid ACK packet, resending last packet.");
+                        resendCurrentSegment = true;
+                    } else {
+                        System.out.println("Server recieved a valid ACK packet.");
+
+                        if (segmentToSend == lastSegment) {
+                            endTime = System.currentTimeMillis();
+                            System.out.println("Transfer time(ms): " + (endTime - startTime));
+                            System.out.println("Retransmissions: " + retransmissions);
+                            System.out.println("Connection terminated.");
+                            socket.close();
+                            return;
+                        }
+
+                        sequenceNumber = (sequenceNumber + 1) % 2;
+                        segmentToSend++;
+                    }
+                } catch (Exception e) {
+                    retransmissions++;
+                    System.out.println("Server timed out, resending last packet.");
+                    resendCurrentSegment = true;
+                }
+
+                if (resendCurrentSegment) {
+                    continue;
+                }
             }
-            // end complete loop if broken.
-            if (quiting)
-                break;
         }
-        System.out.println("Connection terminated.");
-        socket.close();
+    }
+
+    private static void sendPacket(DatagramSocket socket, Packet packet, InetAddress address, int port)
+            throws IOException {
+        byte[] buffer = Packet.packetToString(packet).getBytes();
+        DatagramPacket datagram = new DatagramPacket(buffer, buffer.length, address, port);
+        socket.send(datagram);
+    }
+
+    private static List<String> splitFileIntoSegments(byte[] fileBytes, int payloadSize) {
+        List<String> segments = new ArrayList<>();
+
+        if (fileBytes.length == 0) {
+            segments.add("");
+            return segments;
+        }
+
+        for (int i = 0; i < fileBytes.length; i += payloadSize) {
+            int length = Math.min(payloadSize, fileBytes.length - i);
+            byte[] segmentBytes = new byte[length];
+            System.arraycopy(fileBytes, i, segmentBytes, 0, length);
+            segments.add(Packet.byteToString(segmentBytes));
+        }
+
+        return segments;
     }
 }
